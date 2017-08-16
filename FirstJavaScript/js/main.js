0782003e3150f612d705a68d2d8008c1ba2cff6f
Michael E Miles <michael@miles-me.net>
2017-08-16 02:53:26 -0500

    Programatically created the same index.html file using a .js file instead of imbeded JavaScript (Just learning here guys)

--- FirstJavaScript/js/main.js
+++ FirstJavaScript/js/main.js
@@ -1,3 +1,14 @@
 ﻿// Start of code
-var myText = "Hello, World!";
+var myText = "I have completed chapter 2 and started chapter 3";
 alert(myText);
+
+function showDistance(speed, time) {
+    alert(speed * time);
+};
+
+showDistance(10, 5);
+showDistance(85, 1.5);
+showDistance(22, 9);
+showDistance(42, 21);
+
+alert("Works exactly as I expected");
